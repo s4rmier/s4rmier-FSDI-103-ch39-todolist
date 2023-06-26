5ee2901cc17c342972059a21cfce1d875a8f2a75
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-06-26 11:07:24 -0700

    js -ok, mobile -ok, deploy -ok

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const popUpModal = document.getElementById("popup-modal");
 const modalCloseButton = document.getElementById("modal-close-button");
 const createNewTask = document.getElementById("add-task");
 
+// Step 1: User initiates add task / modal pops up
 createNewTask.addEventListener("click", () => {
   clearInputFields();
   toggleVisibility(popUpModal);
 });
 
+// Outcome a: user closes modal / did not complete task create
 modalCloseButton.addEventListener("click", () => {
   clearInputFields();
   toggleVisibility(popUpModal);
 });
 
+// Step 1b: user press F3 instead of clicking the add task button
 document.addEventListener("keydown", function (event) {
   if (event.key === "F3") {
     clearInputFields();
@@ -24,6 +27,7 @@ const toggleVisibility = (domElement) => {
   domElement.classList.toggle("hidden");
 };
 
+// reset modal values
 const clearInputFields = () => {
   const modalTaskTitle = (document.getElementById("modal-task-title").value =
     "");
@@ -32,6 +36,7 @@ const clearInputFields = () => {
   ).value = "");
 };
 
+// retrieve local time
 const getDate = () => {
   let todaysDate = new Date();
   let months = [
@@ -61,8 +66,19 @@ let modalCreatedDate = (document.getElementById(
   "modal-created-date"
 ).textContent = getDate());
 
+//  step 2: create task
 let modalTaskSubmit = document.getElementById("modal-task-submit");
 
+// add the created and completed tasks into an array
+let taskArr = [];
+let taskCount = document.querySelector(".task-count");
+taskCount.textContent = taskArr.length;
+
+let completedTasks = document.querySelector(".complete-count");
+let completedCounter = 0;
+completedTasks.innerText = completedCounter;
+
+// callback function to retrieve user input and convert them into the list
 modalTaskSubmit.addEventListener("click", () => {
   let newTaskTitle = document.getElementById("modal-task-title").value;
 
@@ -70,11 +86,13 @@ modalTaskSubmit.addEventListener("click", () => {
     "modal-task-description"
   ).value;
 
+  //  input validation / rejects if the fields are empty
   if (newTaskTitle.trim() === "" || newTaskDescription.trim() === "") {
     alert("Invalid input");
     return;
   }
 
+  // dynamically adds a list item into the DOM
   let newTaskItem = document.createElement("li");
   newTaskItem.classList.add("task-item", "card-bg", "flex-col", "justify");
   newTaskItem.innerHTML = `
@@ -84,7 +102,7 @@ modalTaskSubmit.addEventListener("click", () => {
     </h3>
     <h4>
       Created: <span class="created-date">${getDate()}</span>
-    </h4>
+    </h4> 
   </div>
   <p class="task-description">
     ${newTaskDescription}
@@ -94,18 +112,28 @@ modalTaskSubmit.addEventListener("click", () => {
     <h3>Mark Complete</h3>
   </button>`;
 
+  taskArr.push(newTaskTitle);
+  taskCount.textContent = taskArr.length;
+
   let taskList = document.getElementById("task-list");
   let allTasks = document.querySelectorAll(".task-item");
 
+  // if there is an existing task, add the latest task to the very top of the list
   !allTasks.length
     ? taskList.appendChild(newTaskItem)
     : taskList.prepend(newTaskItem);
 
+  // step 3: close the modal / reset modal values
   clearInputFields();
   toggleVisibility(popUpModal);
 
+  // optional step 4:removes the selected task / update the completed tasks counter
   let markCompleteButton = newTaskItem.querySelector(".mark-complete");
   markCompleteButton.addEventListener("click", () => {
+    taskArr = taskArr.filter((item) => item !== newTaskTitle);
+    taskCount.textContent = taskArr.length;
+    completedCounter += 1;
+    completedTasks.innerText = completedCounter;
     newTaskItem.classList.add("hidden");
   });
 });
